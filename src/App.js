7829177958481c013d8d95af95384f3f5c26a7bc
Mahlatse Ngwaile <hlatsengwaile@gmail.com>
2022-10-04 22:37:21 +0200

    added SearchBox to allow user to look up movie

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './Components/MovieList';
+import SearchBox from './components/SearchBox';
+import MovieListHeading from './Components/MovieListHeading';
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
 
   const getMovieRequest = async()=>{
     const url = `http://www.omdbapi.com/?s=star wars&apikey=263d22d8`;
@@ -17,14 +21,20 @@ const App = () => {
 	};
 
 	useEffect(() => {
-		getMovieRequest();
-	}, []);
+		getMovieRequest(searchValue);
+	}, [searchValue]);
 
 	return (
 		<div className='container-fluid movie-app'>
 			<div className='row'>
-				<MovieList movies={movies} />
+				<MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue}
+        setSearchValue={setSearchValue}/>
 			</div>
+
+      <div className='row'>
+        <MovieList movies={movies}/>
+      </div>
 		</div>
 	);
 };
